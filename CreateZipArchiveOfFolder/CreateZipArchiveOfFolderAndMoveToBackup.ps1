5a6ddec3cc7aec2fe9d2446c394f5bc28951dc35
neman <djordjevic.nemanja@gmail.com>
2015-07-26 23:09:47 +0200

    Added CreateZipArchiveOfFolderAndMoveToBackup.ps1

---
+++ CreateZipArchiveOfFolder/CreateZipArchiveOfFolderAndMoveToBackup.ps1
@@ -0,0 +1,24 @@
+﻿# Idea from http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
+if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+$applicationFolder = $PSScriptRoot
+$applicationFolder
+$backupFolder = "D:\Dropbox\DevBox\PowerShell\TestFiles\Backup\"
+$currentDate = (Get-Date).ToString("yyyyMMdd")
+$backupDestination = $backupFolder + $currentDate
+$name =  Split-Path $applicationFolder -Leaf
+
+#Create Backup folder name with current date if does not exist
+#TODO:Create function for this
+if (!(Test-Path $backupDestination)) 
+{
+   New-Item $backupDestination -type directory
+}
+
+Add-Type -assembly System.IO.Compression.Filesystem
+#[io.compression.zipfile]::CreateFromDirectory($applicationFolder, $backupDestination + "\test.zip") 
+[IO.Compression.Zipfile]::CreateFromDirectory($applicationFolder, $backupDestination + "\" + $name + "_" + 
+(Get-Date).TimeOfDay.Hours.ToString() + (Get-Date).TimeOfDay.Minutes.ToString() + ".zip"
+     ,[System.IO.Compression.CompressionLevel]::Optimal,[System.Convert]::ToBoolean("False")) 
+     
+
+     
