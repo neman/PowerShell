b56909af8138668763565d12dc8920b8b73bb542
neman <djordjevic.nemanja@gmail.com>
2015-07-27 00:33:59 +0200

    small refactor

--- CreateZipArchiveOfFolder/CreateZipArchiveOfFolderAndMoveToBackup.ps1
+++ CreateZipArchiveOfFolder/CreateZipArchiveOfFolderAndMoveToBackup.ps1
@@ -1,22 +1,32 @@
 ﻿# Idea from http://blogs.technet.com/b/heyscriptingguy/archive/2015/03/09/use-powershell-to-create-zip-archive-of-folder.aspx
-if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-$applicationFolder = $PSScriptRoot
-$applicationFolder
-$backupFolder = "D:\Dropbox\DevBox\PowerShell\TestFiles\Backup\"
-$currentDate = (Get-Date).ToString("yyyyMMdd")
-$backupDestination = $backupFolder + $currentDate
-$name =  Split-Path $applicationFolder -Leaf
 
-#Create Backup folder name with current date if does not exist
-#TODO:Create function for this
-if (!(Test-Path $backupDestination)) 
+
+if(!$PSScriptRoot)
 {
-   New-Item $backupDestination -type directory
+     #For Powershell v2.0 which does not support $PSScriptRoot introduced with v3.0
+     $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$SourceFolder = $PSScriptRoot
+$SourceFolderName =  Split-Path $SourceFolder -Leaf
+
+#The example in this script is just for the testing purpose, you should use real backup location
+#e.g. $BackupFolder = "c:\Backup"
+$BackupFolder = (Get-Item $SourceFolder).Parent.FullName + "\Backup\"
+$CurrentDate = (Get-Date).ToString("yyyyMMdd")
+#e.g. "C:\Backup\20150501"
+$BackupDestinationFolder = $BackupFolder + $CurrentDate
+
+
+#Create Backup destination folder if it does not exist
+#TODO:Create function for this
+if (!(Test-Path $BackupDestinationFolder)) 
+{
+   New-Item $BackupDestinationFolder -type directory
 }
 
 Add-Type -assembly System.IO.Compression.Filesystem
-#[io.compression.zipfile]::CreateFromDirectory($applicationFolder, $backupDestination + "\test.zip") 
-[IO.Compression.Zipfile]::CreateFromDirectory($applicationFolder, $backupDestination + "\" + $name + "_" + 
+#[io.compression.zipfile]::CreateFromDirectory($myCurrentFolderPath, $BackupDestinationFolder + "\test.zip") 
+[IO.Compression.Zipfile]::CreateFromDirectory($SourceFolder, $BackupDestinationFolder + "\" + $SourceFolderName + "_" + 
 (Get-Date).TimeOfDay.Hours.ToString() + (Get-Date).TimeOfDay.Minutes.ToString() + ".zip"
      ,[System.IO.Compression.CompressionLevel]::Optimal,[System.Convert]::ToBoolean("False")) 
      
